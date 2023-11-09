4337eaa3aa06196c9e2b90b65ba4b06a40c75a8b
abdultalif <abdultalif85@gmail.com>
2023-11-09 14:41:27 +0700

    install prisma

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv/config";
+
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('test');
 });
 
-app.listen('3000', () => {
-    console.log(`port berjalan di http://localhost:3000`);
+app.listen(process.env.port, () => {
+    console.log(`port berjalan di http://localhost:${process.env.port}`);
 });
